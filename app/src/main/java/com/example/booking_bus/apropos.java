5f55aa9e1b753239a9596c6c87b8e550f0104d2b
Estelle2518 <165467240+Estelle2518@users.noreply.github.com>
2024-04-22 08:38:06 +0100

    2nd commit

---
+++ app/src/main/java/com/example/booking_bus/apropos.java
@@ -0,0 +1,14 @@
+package com.example.booking_bus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class apropos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apropos);
+    }
+}
---
+++ app/src/main/java/com/example/booking_bus/modification.java
@@ -0,0 +1,14 @@
+package com.example.booking_bus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class modification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modification);
+    }
+}
