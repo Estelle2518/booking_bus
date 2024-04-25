2e20c93e2a89b766a04772ecf589e040c8743536
Estelle2518 <165467240+Estelle2518@users.noreply.github.com>
2024-04-25 12:11:40 +0100

    3rd commit

--- app/src/main/java/com/example/booking_bus/SettingFragment.java
+++ app/src/main/java/com/example/booking_bus/SettingFragment.java
@@ -1,5 +1,6 @@
 package com.example.booking_bus;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SettingFragment extends Fragment {
 
@@ -14,5 +21,6 @@ public class SettingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_setting, container, false);
+
     }
 }
